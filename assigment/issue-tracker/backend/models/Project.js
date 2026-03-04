253905b5bf206b8d6e82fc8b523db57281f5ddcc
hap4114 <159610291+hap4114@users.noreply.github.com>
2026-03-05 01:12:22 +0530

    Delete assigment/issue-tracker directory

--- assigment/issue-tracker/backend/models/Project.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const projectSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true, trim: true },
-    code: { type: String, required: true, unique: true, uppercase: true, trim: true },
-    description: { type: String, trim: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Project', projectSchema);
